5c7a1d029f41b6da28cf313a515c8100f4bff00a
Guillermo Follana-Berná <g.follana@gmail.com>
2022-08-25 16:32:49 +0200

    Actualizacion
    Mejora del GUI. Genera una tabla para visualizar fácilmente antes de elegir si guardar o no.

--- Buscador.R
+++ Buscador.R
@@ -7,34 +7,35 @@ rm(list=ls(all=TRUE)) # Borrar todo los objetos
 invisible(capture.output(gc())) # Limpiar la memoria
 
 # Instalacion y carga de paquetes de R en bucle
-
 paquetes=c("stringi",
-           "mapview",
            "stringr",
            "rgdal",
            "sf",
            "ggplot2",
            "RColorBrewer",
-           
-)
+           "ggspatial"
+           )
 
 for (i in 1:length(paquetes)) {
-  if (!require(paquetes[i], character.only = TRUE)) install.packages(paquetes[i], character.only = TRUE) 
+  if (!require(paquetes[i], character.only = TRUE)){
+    install.packages(paquetes[i], character.only = TRUE)
+  }  
   library(paquetes[i], character.only = TRUE)
 }
 
-
+# Elegir archivo de busqueda ####
 Archivo=file.choose()
 BD_EAI=read.csv2(Archivo,sep = ";", dec = ",")
 Directorio=paste(strsplit(Archivo,"[\\]")[[1]][1:(length(strsplit(Archivo,"[\\]")[[1]])-1)], collapse ="\\")
 setwd(Directorio)
 load("mapas_obj.RData")
 
-# Formulas a usar ####
+# Funciones a usar ####
 Buscar_Especie = function(x){
   temp=which(str_detect(BD_EAI$scientificname,regex(as.character(x), ignore_case=T))|str_detect(BD_EAI$Specie,regex(as.character(x), ignore_case=T)))
   temp2<<-BD_EAI[temp,]
 }
+
 Buscar_Demarcacion = function(x){
   if (length(strsplit(x,",")[[1]])==5) {
     temp=which(str_detect(BD_EAI$Demarcacion,strsplit(x,",")[[1]][1])
@@ -59,6 +60,7 @@ Buscar_Demarcacion = function(x){
     }
   Tabla<<-BD_EAI[temp,]
 }
+
 Crear_mapa= function(x){
 temp_CAN=which(x$Demarcacion=="CAN")
 temp_Otras=which(x$Demarcacion!="CAN")
@@ -66,55 +68,49 @@ temp_Otras=which(x$Demarcacion!="CAN")
 Can = ggplot(data = Canarias) +
   geom_sf(size = 0.1)+
   geom_sf(aes(fill= Demarcaciones, alpha=0.05), data = demarcaciones, size= 0.50, color= NA)+
-  coord_sf(xlim = c(-19, -13), ylim = c(27,30))+ # zoom hacia la zona del mapa
+  coord_sf(xlim = c(-18.2, -13.5), ylim = c(27.5,29))+ # zoom hacia la zona del mapa
   geom_point(data=x[temp_CAN,], aes(x=Longitud, y=Latitud), 
-             color = "black", size=0.75, shape = 24, fill = "darkred")+
-  scale_y_continuous(breaks=c(27,29,31))+ # modificacion de los ejes
-  scale_x_continuous(breaks=c(-19,-16,-13))+
+             color = "black", size=1.25, shape = 21, fill = "darkred")+
+  scale_x_continuous(breaks=c(-18,-14), labels = paste0(c(18,14), "\u00b0W"))+
+  scale_y_discrete(breaks=28:29,labels = paste0(seq(28,29,1), "\u00b0N"))+
   theme_minimal()+
-  theme(axis.text.x =element_text(size = 3),
+  annotation_scale(pad_x=unit(0.1, "cm"), pad_y=unit(0.1, "cm"), width_hint = 0.2, height = unit(0.1, "cm")) +
+  theme(axis.text.x =element_text(size = 4),
         axis.title.x =element_blank(),
         axis.title.y =element_blank(),
-        axis.text.y=element_text(size = 3),
+        axis.text.y=element_text(size = 4),
         plot.background = element_rect(fill = 'white'),
-        panel.grid.major = element_line(colour = "grey10",size = 0.05),
+        panel.grid.major = element_line(colour = "grey10",size = 0.1555),
         legend.position="none")
 
 Esp = ggplot(data = Spain) +
   geom_sf(size = 0.1)+
   geom_sf(aes(fill= Demarcaciones, alpha=0.05), data = demarcaciones, size= 0.50, color = NA)+
-  coord_sf(xlim = c(-15, 6), ylim = c(35,44))+
+  coord_sf(xlim = c(-20, 6), ylim = c(35,46.5))+
   geom_point(data=x[temp_Otras,], aes(x=Longitud, y=Latitud), 
-             color = "black", size=0.75, shape = 24, fill = "darkred")+
+             color = "black", size=1.25, shape = 21, fill = "darkred")+
+  scale_x_discrete(labels = c("20°W", "15°W", "10°W", "5°W",  "0°", "5°E"))+
+  scale_y_discrete(labels = paste0(seq(36,46,2), "\u00b0N"))+
   theme_minimal()+
-  theme(axis.text.x =element_text(size = 8),
-        axis.title.x =element_blank(),
-        axis.title.y =element_blank(),
-        axis.text.y=element_text(size = 8),
-        plot.background = element_rect(fill = 'white'),
-        legend.position="none")
-
-a=Esp+annotation_custom(grob = ggplotGrob(Can), xmin = -15, xmax = -8, ymin = 34.25, ymax = 38)+
   annotation_scale(location = "br", width_hint = 0.2, height = unit(0.1, "cm")) +
-  annotation_north_arrow(location = "tr", which_north = "true", 
-                         pad_x = unit(0, "in"), pad_y = unit(0.1, "in"),
-                         style = north_arrow_fancy_orienteering(),
-                         height = unit(0.3, "in"),
-                         width = unit(0.3, "in")) +
   ggtitle(unique(x$scientificname)) +
-  theme_minimal()+
-  theme(plot.title = element_text(hjust = 0.5),
+  theme(plot.title = element_text(hjust = 0.5, face="italic"),
         axis.text.x =element_text(size = 4),
         axis.title.x =element_text(size = 5),
         axis.title.y =element_text(size = 5),
         axis.text.y=element_text(size = 4),
         plot.background = element_rect(fill = 'white'),
+        panel.background = element_blank(),
         panel.grid.major = element_line(colour = "grey10",size = 0.05),
         legend.position="none")
+
+a=Esp+annotation_custom(grob = ggplotGrob(Can)
+                      ,xmin = -25, xmax = -5, ymin = 36, ymax = 40
+                      )
 print(a)
 }
 
-
+# GUI del programa ####
 Sys.sleep(1)
 if (interactive() ){
 Nombre <- readline("Cual es tu nombre? ")
@@ -138,17 +134,18 @@ repeat{
   
       if (str_detect(Mapa,regex("si|yes|s|y",ignore_case = T))) {
         Crear_mapa(temp2)
-        # mapa=mapview(temp2[which(!is.na(temp2$Latitud)&!is.na(temp2$Longitud)),], xcol = "Longitud", ycol = "Latitud", crs = 4326, map.types = "CartoDB.Positron", grid = F)
-        # mapa=mapa+mapview(demarcaciones, zcol = "Demarcaciones", col.regions = brewer.pal(5, "Dark2"), alpha.regions = 0.25 ,alpha=0,
-        #          crs = 4326, grid = F)
-        # print(mapa)
         }
       Guardar1 <- readline("Quieres guardar un mapa de la especie? si/no ")
       if (str_detect(Guardar1,regex("si|yes|s|y",ignore_case = T))) {
           #mapshot(mapa, url = paste(temp2[1,1],".html",sep=""))
           ggsave(paste("map_",temp2[1,1],".png",sep = ""), dpi = 500)
           cat(paste("Mapa guardado en ", Directorio , sep=""))
-        }
+      }
+      Ver1 <- readline("Quieres ver una tabla de la especie? si/no ")
+      if (str_detect(Ver1,regex("si|yes|s|y",ignore_case = T))) {
+        View(temp2)
+        cat(paste("Ya puedes ver la tabla de ", Especie, sep=""))
+      }
       Guardar2 <- readline("Quieres guardar una tabla de la especie? si/no ")
       if (str_detect(Guardar2,regex("si|yes|s|y",ignore_case = T))) {
           write.csv2(temp2,file=paste(Especie,".csv",sep=""),row.names = F)
@@ -171,16 +168,16 @@ repeat{
     Mapa=readline("Quieres ver las localizaciones en un mapa? si/no ")
     if (str_detect(Mapa,regex("si|yes|s|y",ignore_case = T))) {
       Crear_mapa(Tabla)
-      # mapa=mapview(Tabla[which(!is.na(Tabla$Latitud)&!is.na(Tabla$Longitud)),], xcol = "Longitud", ycol = "Latitud", crs = 4326, map.types = "CartoDB.Positron", grid = F)
-      # mapa=mapa+mapview(demarcaciones, zcol = "Demarcaciones", col.regions = brewer.pal(5, "Dark2"), alpha.regions = 0.25 ,alpha=0,
-      #                   crs = 4326, grid = F)
-      # print(mapa)
     }
     Guardar1 <- readline("Quieres guardar un mapa de la/s demarcacion/es? si/no ")
     if (str_detect(Guardar1,regex("si|yes|s|y",ignore_case = T))) {
-        #mapshot(mapa, url = paste(toupper(as.character(Demarcacion)),".html",sep=""))
-        ggsave(paste("map_",toupper(as.character(Demarcacion)),".png",sep = ""), dpi = 500)
+        ggsave(paste("map_",toupper(as.character(Demarcacion)),".png",sep = ""), width = 17.6, height = 10,units = "cm")
         cat(paste("Mapa guardado en ", Directorio , sep=""))
+    }
+    Ver1 <- readline("Quieres ver una tabla de la/s demarcacion/es? si/no ")
+    if (str_detect(Ver1,regex("si|yes|s|y",ignore_case = T))) {
+      View(Tabla)
+      cat(paste("Ya puedes ver la tabla de ", Demarcacion , sep=""))
     }
     Guardar2 <- readline("Quieres guardar una tabla de la/s demarcacion/es? si/no ")
     if (str_detect(Guardar2,regex("si|yes|s|y",ignore_case = T))) {
@@ -212,12 +209,4 @@ cat("You entered")
 str(a);
 cat( "\n" )
 }
-
-
-
-
-
-
-
-
 
